65f3a7a224900e03f696d4cf494d29be07287d74
Kauã Rasera <kauarasera@gmail.com>
2025-06-08 09:19:27 +0100

    feat: create PersonRepository with basic name search

---
+++ src/main/java/com/kauarasera/receiptmanager/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.kauarasera.receiptmanager.repository;
+
+import com.kauarasera.receiptmanager.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByFirstNameContainingIgnoreCase(String firstName);
+}
